7f4ff7cb34879be8f4227efd5d06a2539796b657
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-20 20:50:50 -0700

    added links to nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import '../styles/Nav.css'
+import {Link} from 'react-router-dom';
 
 export default function Nav() {
+  const navStyle = {
+    color:'black',
+    textDecoration: "none",
+}
   return (
     <div className='nav'>
       <div className="container">
-        <div className='title'>Dev Blog</div>
-        <div className='button'>All Posts</div>
+        <Link style={navStyle} to='/'>
+          <div className='title'>Dev Blog</div>
+        </Link>
+        <Link to='/'style={navStyle}>
+          <div className='button'>All Posts</div>
+        </Link>
       </div>
     </div>
   )
