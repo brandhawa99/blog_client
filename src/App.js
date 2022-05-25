dd410de482680a8faa129731211f37d5a1d79015
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-24 23:17:00 -0700

    moved components to their own roues

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './styles/App.css';
 import Nav from './components/Nav';
-import Home from './components/Home'
+import Home from './components/Home';
+import Posts from './components/Posts';
 import {BrowserRouter as Router , Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
+    <div className="App">
     <Router>
-      <div className="App">
-        <Nav />  
+    <Nav />  
+      <div className='content'>
         <Routes>
           <Route path='/' element={<Home />} />
-
+          <Route path = '/posts' element={<Posts />} />
+          <Route path = '/posts/:id' element={<Posts />} />
         </Routes>
       </div>
     </Router>
+    </div>
   );
 }
 
