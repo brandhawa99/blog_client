097a6ec9dd38ec33da5876228de7581188a950c0
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-30 16:19:05 -0700

    asdf

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
+import PostHolder from './PostHolder';
+require('../styles/Posts.css')
 
 export default function Posts() {
+  const [posts, setPosts] = useState();
+  const getAllPosts = async() =>{
+    try {
+      const data =  await fetch('http://localhost:3001/posts',{
+        mode:'cors'
+      })
+      if(data.ok){
+        const json = await data.json();
+        setPosts(json);
 
-  //the link needs to be '/post/${post._id}
+      }
+    } catch (error) {
+      
+    }
+  }
+  useEffect(()=>{
+    getAllPosts();
+  },[])
   return (
-    <div>Posts</div>
+    <div className='posts-main'>
+      {
+        (!posts)?<div>There are no posts</div>:
+        <div className='wrapper-grid'> 
+
+        {posts.map(post=>{
+          const {title, author, blog,_id} = post;
+          return <PostHolder key={_id} title={title} author={author} blog={blog} timestamp={new Date(post.timestamp).toISOString().slice(0,10).replace(/-/g,"-")} _id={_id} />
+        })}
+        </div>
+      }
+    </div>
   )
 }
