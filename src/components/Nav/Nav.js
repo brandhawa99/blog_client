c8a3e3c5015297f20367c05e57beee5169dacfde
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-08-21 13:27:57 -0700

    initialized navbar

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Nav.module.css';
+
+function Nav() {
+  return (
+    <div className={styles.mainContainer}>
+      Nav
+
+    </div>
+  );
+};
+
+export default Nav
