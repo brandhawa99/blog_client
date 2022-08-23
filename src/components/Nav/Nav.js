97e1e78b1a7332052e82e50ee9c464227e4c9b53
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-08-23 12:06:59 -0700

    created new nav bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import styles from './Nav.module.css';
+import { NavLink } from 'react-router-dom';
 
 function Nav() {
+  const activeStyle = {
+    backgroundColor: "orangered",
+    color: "black",
+    padding: "7px 10px",
+    borderRadius: ".35rem",
+  }
+
   return (
     <div className={styles.mainContainer}>
-      Nav
-
+        <NavLink 
+            style={({ isActive }) =>
+            isActive ? activeStyle : undefined
+          }
+          className={`${styles.homelink} ${styles.link} `}  to='/'>
+          Dev Blog
+        </NavLink>
+        <NavLink 
+          style={({ isActive }) =>
+          isActive ? activeStyle : undefined
+        }
+        className={styles.link} to='/posts'>
+        posts
+        </NavLink>
     </div>
   );
 };
