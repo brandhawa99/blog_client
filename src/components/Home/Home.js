8df05932caaf0e775f17a3982150c3a7f31a980e
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-08-23 12:07:41 -0700

    added fetch

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import {React, useEffect, useState} from 'react';
 import styles from './Home.module.css';
+import hero from '../../icons/hero.svg';
+import PostHolder from '../PostHolder';
 
-const Home = () => (
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  //get the 5 most recent post from database
+  const fetch_index = async() =>{
+    const data = await fetch('https://agile-mesa-41864.herokuapp.com/')
+    const post = await data.json();
+    setPosts(post);
+  }
+
+  useEffect(()=>{
+    fetch_index();
+
+  },[])  
+  
+
+  return (
   <div className={styles.Home} data-testid="Home">
-    Home Component
+    <div className={styles.headerContent}>
+      <div className={styles.text}>
+        <h1>Welcome To The Dev Blog</h1>
+        <p>
+          This is where anyone and eveyone who is anybody comes to 
+          get the latest news in tech. First, factual news! Come and
+           read one of our wonderful articles! 
+        </p>
+      </div>
+      <img className={styles.heroIcon} src={hero} alt="hero icon"></img>
+    </div>
+    <div className={styles.posts}>
+    {
+        posts.map(post =>{
+          const {title, author, blog,_id} = post;
+          return <PostHolder key={_id} title={title} author={author} blog={blog} timestamp={post.timestamp} _id={_id} />
+        })
+      }
+    </div>
+
   </div>
-);
-
-Home.propTypes = {};
-
-Home.defaultProps = {};
+)
+};
 
 export default Home;
