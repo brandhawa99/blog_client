ca240716111027f7f5ae6e5e923e69f8c07d73bf
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-08-23 12:07:32 -0700

    changed component location

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './styles/App.css';
-import Nav from './components/Nav';
-import Home from './components/Home';
+import Nav from './components/Nav/Nav';
+import Home from './components/Home/Home';
 import Posts from './components/Posts';
 import Post from './components/Post'
 import {BrowserRouter as Router , Route, Routes} from 'react-router-dom';
