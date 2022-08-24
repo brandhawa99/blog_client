244ce850a14a798f0934593ecddea44c4f3e9395
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-08-23 21:15:38 -0700

    card

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -2,13 +2,13 @@ import React from 'react';
 import styles from './PostCard.module.css';
 
 function PostCard(props) {
-  const {author, title , date} = props; 
+  const {author, title , date, _id} = props; 
   return (
-    <div className={styles.mainContainer}>
+    <div key={_id} className={styles.mainContainer}>
       <div className={styles.title}>{title}</div>
       <div className={styles.dataContainer}>
         <div>{date}</div>
-        <div>{author}</div>
+        <div>{author.first_name}, {author.last_name}</div>
       </div>
     </div>
   );
